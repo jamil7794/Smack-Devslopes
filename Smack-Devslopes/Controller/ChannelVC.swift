64fcc97a153ea8979afc1f6448ac2c48c072c489
JamilJalal <jamil.jalal94@gmail.com>
2018-07-07 14:18:51 -0400

    Test

--- Smack-Devslopes/Controller/ChannelVC.swift
+++ Smack-Devslopes/Controller/ChannelVC.swift
@@ -10,20 +10,29 @@ import UIKit
 
 class ChannelVC: UIViewController {
     static let instance = ChannelVC()
-    @IBOutlet weak var loginBtn: UIButton?
+    @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareforUnwind(segue: UIStoryboardSegue){}
     
+    @IBOutlet weak var userImg: CircleImage!
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+            // Whenever we create an account the notification is posted. this function obsevers the notification when it is posted then it performs a task
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-       loginBtn?.setTitle("\(userDataService.instance.name)", for: .normal)
-    }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
     
+    @objc func userDataDidChange(_ notif: Notification){ //78
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(userDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: userDataService.instance.avatarName)
+        }else{
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
+    }
 }
